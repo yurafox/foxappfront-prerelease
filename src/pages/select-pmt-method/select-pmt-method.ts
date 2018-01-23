d40c9e4bf6071ba7995c3e8a0a358f5c3655f6ec
yurafox <y_p_i@yahoo.com>
2018-01-23 20:22:59 +0200

    select-pmt-method: completed PersonalInfo form

--- src/pages/select-pmt-method/select-pmt-method.ts
+++ src/pages/select-pmt-method/select-pmt-method.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import {ChangeDetectorRef, Component, ViewChild} from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import {ComponentBase} from '../../components/component-extension/component-base';
 import {EnumPaymentMethod} from '../../app/model/enum-payment-method';
 import {AbstractDataRepository} from '../../app/service/repository/abstract/abstract-data-repository';
 import {CartService} from '../../app/service/cart-service';
 import {PersonInfo} from '../../app/model/person';
+import {NgForm} from '@angular/forms';
 
 
 @IonicPage()
@@ -14,11 +15,13 @@ import {PersonInfo} from '../../app/model/person';
 })
 export class SelectPmtMethodPage extends ComponentBase {
 
+  @ViewChild('f') personInfoEditForm: NgForm;
   pmtMethods = new Array <{isChecked: boolean, method: EnumPaymentMethod}>();
   dataLoaded = false;
 
   constructor(public navCtrl: NavController, public navParams: NavParams,
-              public repo: AbstractDataRepository, public cart: CartService) {
+              public repo: AbstractDataRepository, public cart: CartService,
+              private cdRef:ChangeDetectorRef) {
     super();
     this.getPmtMethods();
   }
@@ -43,9 +46,18 @@ export class SelectPmtMethodPage extends ComponentBase {
     return res;
   }
 
+  personInfoValid(): boolean {
+    return !((this.personInfoEditForm) && (!this.personInfoEditForm.valid));
+  }
+
+  validatePage() : boolean {
+    return (this.isAnyOptionSelected() && this.personInfoValid());
+  }
+
   onSelectOptionClick(option) {
     this.pmtMethods.forEach(i => {
         i.isChecked = (i === option);
+        this.cdRef.detectChanges();
         this.cart.pmtMethod = option.method;
       }
     );
