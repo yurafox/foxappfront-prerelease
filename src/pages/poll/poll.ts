5ec7753171e7d182178c48be52e46911c68d3094
sergce <sergceinfo@gmail.com>
2018-01-24 18:50:22 +0200

    add view details

--- src/pages/poll/poll.ts
+++ src/pages/poll/poll.ts
@@ -1,8 +1,14 @@
+import { IDictionary } from './../../app/core/app-core';
 import { PollQuestion, PollQuestionAnswer, AnswerType } from './../../app/model/index';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { ComponentBase } from '../../components/component-extension/component-base';
 import { AbstractDataRepository } from '../../app/service/index';
+
+interface IPollResult {
+  questionId:number,
+  answerValue:string
+}
 
 @IonicPage()
 @Component({
@@ -12,6 +18,8 @@ import { AbstractDataRepository } from '../../app/service/index';
 export class PollPage extends ComponentBase{
   private pollId:number;
   private pollQuestions:Array<PollQuestion>=[];
+  private pollresults:{pollId:number, pollResult:IDictionary<IPollResult>};
+
   public pollQuestAns:Array<{questionObj:PollQuestion, answers:PollQuestionAnswer[]}>=[];
 
   constructor(public navCtrl: NavController,
@@ -19,6 +27,7 @@ export class PollPage extends ComponentBase{
               public _repo:AbstractDataRepository) {
     super();
     this.pollId = this.navParams.data.id;
+    this.pollresults = {pollId:this.pollId,pollResult:{}};
   }
 
   async ngOnInit(){
@@ -41,5 +50,13 @@ export class PollPage extends ComponentBase{
 
   public get listTextType():AnswerType {
     return AnswerType.ListText;
+  }
+
+  public onSelectionChange(questionId,answerValue):void {
+    this.pollresults.pollResult[`${questionId}`] = {questionId:questionId,answerValue:`${answerValue}`};
+  }
+
+  public onSelectionOwnerChange(questionId):void {
+     console.log(questionId);
   }
 }
