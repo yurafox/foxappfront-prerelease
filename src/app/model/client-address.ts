8d7eafa8e1c0c8da55245e261bb3656b6fcc283b
yurafox <y_p_i@yahoo.com>
2018-03-04 18:37:25 +0200

    Run this: npm install ngx-lazy-load-images --save
    This package adds images lazy loading feature used in item component

--- src/app/model/client-address.ts
+++ src/app/model/client-address.ts
@@ -26,14 +26,12 @@ export class ClientAddress {
   }
 
   public get addressString(): string {
-    const cntry = ((<any>this).country) ? (<any>this).country.name : '';
-    const phone = this.phone ? ', Phone: ' + this.phone : '';
     return ((this.street) ? this.street : '') + ' ' +
       ((this.bldApp) ? this.bldApp : '') + ', ' +
       ((this.city) ? this.city : '') + ', ' +
       ((this.zip) ? this.zip : '') + ' ' +
-      cntry + ' ' +
-      phone;
+      (((<any>this).country) ? (<any>this).country.name : '') + ' ' +
+      ((this.phone) ? ', Phone: ' + this.phone : '');
   }
 
   constructor (
