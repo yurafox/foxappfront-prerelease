4d9ab4bfbbd9e6b9e1dc09cd2091cd147751b838
sergey <sergceinfo@gmail.com>
2018-07-02 11:11:49 +0300

    changed category-btn for fast display block

--- src/components/category-btn/category-btn.ts
+++ src/components/category-btn/category-btn.ts
@@ -9,22 +9,33 @@ import {AbstractDataRepository} from '../../app/service/repository/abstract/abst
 })
 export class CategoryBtnComponent extends ComponentBase {
   public countStr:string;
-  public get productCountInOneCatalog():string {
-     if(this.locale['CategoryCount'] && this.countStr)
-       return this.locale['CategoryCount'].replace('${countStr}',this.countStr);
-  }
+  public productCountInOneCatalog:string;
 
   constructor(public navCtrl: NavController, public _repo:AbstractDataRepository) {
         super();
-        this.initLocalization();
   }
 
   async ngOnInit() {
     super.ngOnInit();
     this.countStr = await this._repo.getAppParam('CATEGORY_COUNT');
+    this.productCountInOneCatalog = (this.locale['CategoryCount'] && this.countStr) 
+                              ? this.locale['CategoryCount'].replace('${countStr}',this.countStr)
+                              : this.callDisplayBlank();
   }
 
-  toCategories():void{
+  toCategories():void {
     this.navCtrl.push('CategoryTreePage').catch((err)=>console.error(err));
+  }
+
+  callDisplayBlank():string {
+      const lang:number = this.localeID;
+      let res:string;
+      switch (lang) {
+        case 0:{res=`more than ${this.countStr} products in catalog`;break;}
+        case 2:{res =`більше ${this.countStr} товарів в каталозі`;break;}
+        default:{res=`более ${this.countStr} товаров в каталоге`;}
+      }
+
+      return res;
   }
 }
