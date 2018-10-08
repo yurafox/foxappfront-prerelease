af1fe33bfaf0e0b78e1d3fe9d1c88251ea2e52ce
vargav <dealio07@gmail.com>
2018-10-08 16:48:52 +0300

    encapsulated item-description styles

--- src/components/item-description/item-description.ts
+++ src/components/item-description/item-description.ts
@@ -1,19 +1,36 @@
-import {Component, Input} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, Input, ViewEncapsulation} from '@angular/core';
 import {ComponentBase} from '../component-extension/component-base';
-
 
 @Component({
   selector: 'item-description',
-  templateUrl: 'item-description.html'
+  templateUrl: 'item-description.html',
+  encapsulation: ViewEncapsulation.Native
 })
-export class ItemDescriptionComponent extends ComponentBase {
+export class ItemDescriptionComponent extends ComponentBase implements AfterViewInit {
 
   @Input() description: string;
   @Input() displayTextLength: number;
 
-  constructor() {
+  constructor(private itemDescrElem: ElementRef) {
     super();
   }
 
+  ngAfterViewInit() {
+    if (this.itemDescrElem && this.itemDescrElem.nativeElement) {
+      let images: any;
+      if (this.itemDescrElem.nativeElement.shadowRoot) {
+        images = this.itemDescrElem.nativeElement.shadowRoot.querySelectorAll("img");
+      }
+      else {
+        images = this.itemDescrElem.nativeElement.querySelectorAll("img");
+      }
+      if (images && images.length > 0) {
+        for (let img of images) {
+          img.parentElement.style.maxWidth = window.innerWidth+"px";
+          img.style.maxWidth = window.innerWidth+"px";
+        }
+      }
+    }
+  }
 
 }
